07d228e1016aee65abd2d78193e3d19bac7c3656
Djunquera <47776320+Djunquera@users.noreply.github.com>
2019-05-21 22:04:30 +0200

    Update programa.c

--- programa.c
+++ programa.c
@@ -2,6 +2,7 @@
 //necesario para system("cls")
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 #include "myLib.h"
 
 
@@ -13,10 +14,12 @@ int main()
 	FILE *pf2;
 	FILE *pf3;
 	FILE *pf4;
+	FILE *pf5;
 
 
 int i=0;
 
+system("COLOR 3F");
 //asignamos componente a las estructuras
 //alumno esta asociado a la estructura usuario
 usuario alumno;
@@ -25,8 +28,10 @@ contenido fichero;
 
 opciones menu;
 
+clase etsidi;
 
-/*el usuario debera dar su nombre,apellidos, nº de matricula y carrera 
+
+/*el usuario debera dar su nombre,apellidos, nÂº de matricula y carrera 
 (estos corresponden a los siguientes enteros y strings definidos)*/
 
 /*bucle do while para que se repite siempre el menu hasta
@@ -58,7 +63,7 @@ switch(menu.opcion)
 		fprintf(pf3, "%i,",alumno.matricula);
 			fclose(pf3);
 	
-	//En esta función se guardan el nombre e apellido en un fichero y se cuenta la longitud de estas para meterlas en otro fichero
+	//En esta funciÃ³n se guardan el nombre e apellido en un fichero y se cuenta la longitud de estas para meterlas en otro fichero
 	guarda_nomb_apelli(alumno,pf1,pf2);
 
 	//Aqui se realiza la seleccion de la carrera por parte del usuario y sera guardado en un fichero
@@ -73,22 +78,22 @@ switch(menu.opcion)
 	 el conjunto de datos de un un usuario a su matricula*/
 
 	/*Si la matricula no esta registrada el programa vuelve a enviar al usuario al menu principal,
-	si esta registrada se mantiene en el case 2 y abrirá otro switch-case*/
+	si esta registrada se mantiene en el case 2 y abrirÃ¡ otro switch-case*/
 	
-	/*posicion nos dará la posición de la matricula dentro del vector matricula (y la posicion del nº asociado a la carrera)
-	matri es la matricula que va a escribir el usuario para comparar con las matrículas ya registradas*/
+	/*posicion nos darÃ¡ la posiciÃ³n de la matricula dentro del vector matricula (y la posicion del nÂº asociado a la carrera)
+	matri es la matricula que va a escribir el usuario para comparar con las matrÃ­culas ya registradas*/
 	int posicion;
 	
 	menu.opcion=matriculaexistente(alumno,fichero,menu,&posicion,pf3);
 
-	//if descrito en la funcion anterior que solo se cumple cuando la matrícula esta registrada
+	//if descrito en la funcion anterior que solo se cumple cuando la matrÃ­cula esta registrada
 	if(menu.opcion==2)
 	{
 	
 	
 	do{
 	system("cls");
-	printf("(1)Ver mi ficha\n(2)Donde hay clase\n(3)Tutorias con mis profes\n(4)Volver al menu principal\n");
+	printf("(1)Ver mi ficha\n(2)Donde hay clase\n(3)Tutorias con mis profes\n(4)Saber dia y hora\n(5)Volver al menu principal\n");
 	scanf("%i",&menu.segundaopcion);
 	
 		switch (menu.segundaopcion)
@@ -118,7 +123,8 @@ switch(menu.opcion)
 							{
 							
 								system("cls");
-							printf("El programa muestra donde tiene clase en ese momento el alumno en un mapa");
+							clases(etsidi,pf5);
+							printf("");
 							getch();
 						}
 							break;
@@ -131,7 +137,7 @@ switch(menu.opcion)
 						case 3:
 							{
 								system("cls");
-								printf("Permite volver al menu anterior");
+								printf("Estas volviendo al menu anterior");
 								sleep(2);
 							}
 							break;
@@ -158,7 +164,34 @@ switch(menu.opcion)
 			case 4:
 				{
 					system("cls");
-					printf("Permite volver al menu anterior");
+					
+		//funciona como reloj que nos da la hora local
+
+		/* funcion time que recibe como parametro un puntero nulo
+		 --> devuelve el tiempo en una variable de tipo time_t*/
+ 		 time_t t=time(0);
+  		/*funcion localtime recime como parametro un puntero 
+  		t de variable time_t --> devuelve un puntero (horalocal) hacia la estructura de tipo tm*/
+  		struct tm *local=localtime(&t);
+  		//definimos una cadena de caracteres dodne se va a almacenar la informacion final
+  		char hora[10];
+ 		 char dia[10];
+  
+  		/*funcion strftime con 1Âº parametro la cadena definida,2Âº su longitud,
+  		3Âº cadena de formateo implementada con #include <time.h>  
+	  	y 4Âº el puntero que seÃ±ala la estructura que contiene el tiempo que queremos formatear*/
+	  strftime(hora, 99, "%H:%M:%S",local );
+		  strftime(dia,9,"%A",local);
+ 		 //finalmente imprimimos la cadena de caracteres hora
+  		printf ("\nHora: %s\n\nDia: %s\n", hora,dia);
+		//asociamos a la carrera del alumno su horario para poder mostrarle sus clases		
+		getch();
+				}
+			break;
+			case 5:
+				{
+					system("cls");
+					printf("Estas volviendo al menu anterior");
 					sleep(2);
 				}
 				break;
@@ -171,7 +204,7 @@ switch(menu.opcion)
 				break;
 					}
 				
-	}while(menu.segundaopcion!=4);
+	}while(menu.segundaopcion!=5);
 }
 
 	
@@ -181,7 +214,7 @@ switch(menu.opcion)
 	{
 	system("cls");
 	//aqui el programa presenta un poco en que consiste dicho programa
-	printf("TEXTACO DE EXPLICACION\n");
+	printf("Bienvenido al programa ETSIDI Maps, este programa te permitira\nen un primer lugar crear un usuario, e iniciar sesion en ocasiones\nposteriores. Tendras asi acceso a tus datos y, depiendo  de tu carrera,\na la localizacion de tus futuras clases o los despachos de tus profesores.\n");
 	//sleep usado para que el mensaje siguiente se vea mas estetico al aparecer mas tarde
 	sleep(2);
 	printf ("\n\t\t\tPress any key to go back to menu\n");
@@ -203,27 +236,5 @@ switch(menu.opcion)
 
 
 
-//funciona como reloj que nos da la hora local
-
-/* funcion time que recibe como parametro un puntero nulo
- --> devuelve el tiempo en una variable de tipo time_t*/
-  time_t t=time(0);
-  /*funcion localtime recime como parametro un puntero 
-  t de variable time_t --> devuelve un puntero (horalocal) hacia la estructura de tipo tm*/
-  struct tm *local=localtime(&t);
-  //definimos una cadena de caracteres dodne se va a almacenar la informacion final
-  char hora[10];
-  char dia[10];
-  
-  /*funcion strftime con 1º parametro la cadena definida,2º su longitud,
-  3º cadena de formateo implementada con #include <time.h>  
-  y 4º el puntero que señala la estructura que contiene el tiempo que queremos formatear*/
-  strftime(hora, 99, "%H:%M:%S",local );
-  strftime(dia,9,"%A",local);
-  //finalmente imprimimos la cadena de caracteres hora
-  printf ("Son las: %s\ndia %s\n", hora,dia);
-//asociamos a la carrera del alumno su horario para poder mostrarle sus clases
-
 return 0;
 }
-
